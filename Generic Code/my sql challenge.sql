8c9f0e15c908509bf20d1f0887f0cda1a736a2ee
M-Bahy <Homelander.SolderBoy@gmail.com>
2024-08-22 00:22:28 +0300

    mysql rename

--- Generic Code/my sql challenge.sql
+++ Generic Code/my sql challenge.sql
@@ -1,22 +1,22 @@
 create database bahy
 drop database bahy
 use bahy
-select count(*) from original where value < 30 or value >50
-select count(*) from original where (value < 30 or value >50) and server =1
-select count(*) from original where (value < 30 or value >50) and server =2
-select count(*) from original where (value < 30 or value >50) and server =3
-truncate table compact
+select count(*) from rn_qos_data_0018 where samplevalue < 30 or samplevalue >50
+select count(*) from rn_qos_data_0018 where (samplevalue < 30 or samplevalue >50) and table_id =1
+select count(*) from rn_qos_data_0018 where (samplevalue < 30 or samplevalue >50) and table_id =2
+select count(*) from rn_qos_data_0018 where (samplevalue < 30 or samplevalue >50) and table_id =3
+truncate table compact_0018
 select * from rn_qos_data_0018
-select sum(Value) from original where time < "2024-01-01 01:00:00" and server = 1
-select * from original where time < "2024-01-01 01:00:00" and server = 1
+select table_id , samplesampletime , samplesamplevalue from rn_qos_data_0018
+select sum(samplevalue) from rn_qos_data_0018 where sampletime < "2024-01-01 01:00:00" and table_id = 1
+select * from rn_qos_data_0018 where sampletime < "2024-01-01 01:00:00" and table_id = 1
 
-select sum(Value) from original where time >= "2024-01-01 01:00:00" and  time <= "2024-01-01 01:55:00" and server = 1
-select * from original where time >= "2024-01-01 01:00:00" and  time <= "2024-01-01 01:55:00" and server = 1
+select sum(samplevalue) from rn_qos_data_0018 where sampletime >= "2024-01-01 01:00:00" and  sampletime <= "2024-01-01 01:55:00" and table_id = 1
+select * from rn_qos_data_0018 where sampletime >= "2024-01-01 01:00:00" and  sampletime <= "2024-01-01 01:55:00" and table_id = 1
 
 
-select * from compact_0018
-truncate table original
-drop table original 
-truncate table compact
-ALTER TABLE original
-ADD CONSTRAINT pk_server_time PRIMARY KEY (Server, Time);
+select * from compact_0018_0018
+truncate table rn_qos_data_0018
+drop table rn_qos_data_0018 
+truncate table compact_0018
+ALTER TABLE rn_qos_data_0018
