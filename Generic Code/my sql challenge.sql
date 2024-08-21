db790d6c5525cc7fe0ae330a39dc2c76b3e49772
M-Bahy <Homelander.SolderBoy@gmail.com>
2024-08-21 23:40:36 +0300

    real original table name

--- Generic Code/my sql challenge.sql
+++ Generic Code/my sql challenge.sql
@@ -6,7 +6,7 @@ select count(*) from original where (value < 30 or value >50) and server =1
 select count(*) from original where (value < 30 or value >50) and server =2
 select count(*) from original where (value < 30 or value >50) and server =3
 truncate table compact
-select * from original
+select * from rn_qos_data_0018
 select sum(Value) from original where time < "2024-01-01 01:00:00" and server = 1
 select * from original where time < "2024-01-01 01:00:00" and server = 1
 
